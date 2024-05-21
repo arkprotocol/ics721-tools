4d395c3375412262bc89f4b84e2164be922f5a8d
mr-t <mr-t@arkprotocol.io>
2024-05-21 13:53:56 +0200

    use get_attribute_by_tx

--- cli/create-collection.sh
+++ cli/create-collection.sh
@@ -8,25 +8,60 @@ function create_collection() {
     ARGS=$@
     while [[ "$#" -gt 0 ]]; do
         case $1 in
-            --chain) CHAIN=""${2,,}""; shift ;; # lowercase
-            --name) NAME="$2"; shift ;;
-            --data) DATA="$2"; shift ;;
-            --symbol) SYMBOL="$2"; shift ;;
-            --uri) URI="$2"; shift ;;
-            --label) LABEL="$2"; shift ;; # WASM
-            --code-id) CODE_ID="${2}"; shift ;;
-            --collection) COLLECTION="$2"; shift ;; # NFT module
-            --description) DESCRIPTION="$2"; shift ;; # NFT module
-            --description=*) DESCRIPTION="${1:14}" ;; # NFT module
-            --from) FROM="$2"; shift ;;
-            --admin) ADMIN="$2"; shift ;;
-            *) echo "Unknown parameter: $1, args passed: '$ARGS'" >&2; return 1 ;;
+        --chain)
+            CHAIN=""${2,,}""
+            shift
+            ;; # lowercase
+        --name)
+            NAME="$2"
+            shift
+            ;;
+        --data)
+            DATA="$2"
+            shift
+            ;;
+        --symbol)
+            SYMBOL="$2"
+            shift
+            ;;
+        --uri)
+            URI="$2"
+            shift
+            ;;
+        --label)
+            LABEL="$2"
+            shift
+            ;; # WASM
+        --code-id)
+            CODE_ID="${2}"
+            shift
+            ;;
+        --collection)
+            COLLECTION="$2"
+            shift
+            ;; # NFT module
+        --description)
+            DESCRIPTION="$2"
+            shift
+            ;;                                    # NFT module
+        --description=*) DESCRIPTION="${1:14}" ;; # NFT module
+        --from)
+            FROM="$2"
+            shift
+            ;;
+        --admin)
+            ADMIN="$2"
+            shift
+            ;;
+        *)
+            echo "Unknown parameter: $1, args passed: '$ARGS'" >&2
+            return 1
+            ;;
         esac
         shift
     done
 
-    if [ -z $CHAIN ]
-    then
+    if [ -z $CHAIN ]; then
         echo "--chain is required" >&2
         return 1
     fi
@@ -34,29 +69,24 @@ function create_collection() {
     ark select chain "$CHAIN"
     EXIT_CODE=$?
     if [ "$EXIT_CODE" -ne 0 ]; then
-        return $EXIT_CODE;
+        return $EXIT_CODE
     fi
 
-    if [ -z $FROM ]
-    then
+    if [ -z $FROM ]; then
         echo "--from is required" >&2
         return 1
     fi
 
-    if [ "$ICS721_MODULE" == wasm ]
-    then
-        if [ -z "$CODE_ID" ]
-        then
+    if [ "$ICS721_MODULE" == wasm ]; then
+        if [ -z "$CODE_ID" ]; then
             echo "--code-id is required" >&2
             return 1
         fi
-        if [ -z $LABEL ]
-        then
+        if [ -z $LABEL ]; then
             echo "--label is required on all contracts!" >&2
             return 1
         fi
-        if [ -z $SYMBOL ]
-        then
+        if [ -z $SYMBOL ]; then
             echo "--symbol is required!" >&2
             return 1
         fi
@@ -69,43 +99,38 @@ function create_collection() {
 --gas-prices $CLI_GAS_PRICES --gas $CLI_GAS --gas-adjustment $CLI_GAS_ADJUSTMENT \
 -b $CLI_BROADCAST_MODE --yes \
 --node $CHAIN_NODE --chain-id $CHAIN_ID" \
-"$( [ ! -z $ADMIN ] && echo "--admin $ADMIN" || echo "--no-admin")"
+            "$([ ! -z $ADMIN ] && echo "--admin $ADMIN" || echo "--no-admin")"
 
-        CMD_OUTPUT=`execute_cli "$CMD"`
+        CMD_OUTPUT=$(execute_cli "$CMD")
         EXIT_CODE=$?
-        if [ $EXIT_CODE != 0 ]
-        then
+        if [ $EXIT_CODE != 0 ]; then
             echo "$QUERY_OUTPUT" >&2
             return "$EXIT_CODE" >&2
         fi
-        TXHASH=`echo $CMD_OUTPUT | jq -r '.data.txhash'`
-        if [ -z "$TXHASH" ] && [ "$TXHASH" = null ]
-        then
+        TXHASH=$(echo $CMD_OUTPUT | jq -r '.data.txhash')
+        if [ -z "$TXHASH" ] && [ "$TXHASH" = null ]; then
             echo "ERROR no tx found!" >&2
             echo $CMD_OUTPUT >&2
             return 1
         fi
 
         # query tx for making sure it succeeds!
-        QUERY_OUTPUT=`query_tx --chain $CHAIN --tx $TXHASH --max-call-limit $MAX_CALL_LIMIT`
+        QUERY_OUTPUT=$(query_tx --chain $CHAIN --tx $TXHASH --max-call-limit $MAX_CALL_LIMIT)
         EXIT_CODE=$?
-        if [ $EXIT_CODE != 0 ]
-        then
+        if [ $EXIT_CODE != 0 ]; then
             echo "$QUERY_OUTPUT" >&2
             return "$EXIT_CODE"
         fi
         echo "retrieving collection address" >&2
-        COLLECTION=`echo $QUERY_OUTPUT| jq '.data.logs[0].events[] | select(.type == "instantiate") | .attributes[] | select(.key =="_contract_address")' | jq -r '.value'`
+        COLLECTION=$(get_attribute_by_tx $TXHASH --event-type instantiate)
         echo "collection address: $COLLECTION" >&2
-        INITIAL_CMD=`echo $CMD_OUTPUT | jq -r '.cmd' | sed 's/"/\\\\"/g'` # escape double quotes
-        RESULT=`echo $QUERY_OUTPUT | jq "{ cmd: \"$INITIAL_CMD\", data: .data, id: \"$COLLECTION\"}"`
+        INITIAL_CMD=$(echo $CMD_OUTPUT | jq -r '.cmd' | sed 's/"/\\"/g') # escape double quotes
+        RESULT=$(echo $QUERY_OUTPUT | jq "{ cmd: \"$INITIAL_CMD\", data: .data, id: \"$COLLECTION\"}")
         echo $RESULT | jq
         return 0
     else
-        if [ "$ICS721_MODULE" = nft ]
-        then
-            if [ -z "$COLLECTION" ]
-            then
+        if [ "$ICS721_MODULE" = nft ]; then
+            if [ -z "$COLLECTION" ]; then
                 echo "--collection is required" >&2
                 return 1
             fi
@@ -121,42 +146,37 @@ function create_collection() {
 --from "$FROM" \
 --fees "$CLI_FEES" \
 -b "$CLI_BROADCAST_MODE" --yes" \
-"$( [ ! -z "$SYMBOL" ] && echo "--symbol \"$SYMBOL\"" || echo "")" \
-"$( [ ! -z "$DATA" ] && echo "--data '$DATA'" || echo "")" \
-"$( [ ! -z "$NAME" ] && echo "--name \"$NAME\"" || echo "")" \
-"$( [ ! -z "$URI" ] && echo "--uri \"$URI\"" || echo "")" \
-"$( [ ! -z "$DESCRIPTION" ] && echo "--description=\"$DESCRIPTION\"" || echo "")"
-            CMD_OUTPUT=`execute_cli "$CMD"`
+                "$([ ! -z "$SYMBOL" ] && echo "--symbol \"$SYMBOL\"" || echo "")" \
+                "$([ ! -z "$DATA" ] && echo "--data '$DATA'" || echo "")" \
+                "$([ ! -z "$NAME" ] && echo "--name \"$NAME\"" || echo "")" \
+                "$([ ! -z "$URI" ] && echo "--uri \"$URI\"" || echo "")" \
+                "$([ ! -z "$DESCRIPTION" ] && echo "--description=\"$DESCRIPTION\"" || echo "")"
+            CMD_OUTPUT=$(execute_cli "$CMD")
             EXIT_CODE=$?
-            if [ $EXIT_CODE != 0 ]
-            then
+            if [ $EXIT_CODE != 0 ]; then
                 echo "$QUERY_OUTPUT" >&2
                 return "$EXIT_CODE" >&2
             fi
-            TXHASH=`echo $CMD_OUTPUT | jq -r '.data.txhash'`
-            if [ -z "$TXHASH" ] && [ "$TXHASH" = null ]
-            then
+            TXHASH=$(echo $CMD_OUTPUT | jq -r '.data.txhash')
+            if [ -z "$TXHASH" ] && [ "$TXHASH" = null ]; then
                 echo "ERROR no tx found!" >&2
                 echo $CMD_OUTPUT >&2
                 return 1
             fi
 
             # query tx for making sure it succeeds!
-            QUERY_OUTPUT=`query_tx --chain $CHAIN --tx $TXHASH --max-call-limit $MAX_CALL_LIMIT`
+            QUERY_OUTPUT=$(query_tx --chain $CHAIN --tx $TXHASH --max-call-limit $MAX_CALL_LIMIT)
             EXIT_CODE=$?
-            if [ $EXIT_CODE != 0 ]
-            then
+            if [ $EXIT_CODE != 0 ]; then
                 echo "$QUERY_OUTPUT" >&2
                 return "$EXIT_CODE" >&2
             fi
-            INITIAL_CMD=`echo $CMD_OUTPUT | jq -r '.cmd' | sed 's/"/\\\\"/g'` # escape double quotes
-            RESULT=`echo $QUERY_OUTPUT | jq "{ cmd: \"$INITIAL_CMD\", data: .data, id: \"$COLLECTION\"}"`
+            INITIAL_CMD=$(echo $CMD_OUTPUT | jq -r '.cmd' | sed 's/"/\\\\"/g') # escape double quotes
+            RESULT=$(echo $QUERY_OUTPUT | jq "{ cmd: \"$INITIAL_CMD\", data: .data, id: \"$COLLECTION\"}")
             echo $RESULT | jq
             return 0
-        elif [ "$ICS721_MODULE" = collection ]
-        then
-            if [ -z "$COLLECTION" ]
-            then
+        elif [ "$ICS721_MODULE" = collection ]; then
+            if [ -z "$COLLECTION" ]; then
                 echo "--collection is required" >&2
                 return 1
             fi
@@ -169,44 +189,39 @@ function create_collection() {
 --from $FROM \
 --fees $CLI_MAX_FEES \
 -b $CLI_BROADCAST_MODE --yes" \
-"$( [ ! -z $SYMBOL ] && echo "--symbol $SYMBOL" || echo "")" \
-"$( [ ! -z "$NAME" ] && echo "--name \"$NAME\"" || echo "")"
+                "$([ ! -z $SYMBOL ] && echo "--symbol $SYMBOL" || echo "")" \
+                "$([ ! -z "$NAME" ] && echo "--name \"$NAME\"" || echo "")"
 
-            CMD_OUTPUT=`execute_cli "$CMD"`
+            CMD_OUTPUT=$(execute_cli "$CMD")
             EXIT_CODE=$?
-            if [ $EXIT_CODE != 0 ]
-            then
+            if [ $EXIT_CODE != 0 ]; then
                 echo "$QUERY_OUTPUT" >&2
                 return "$EXIT_CODE" >&2
             fi
-            TXHASH=`echo $CMD_OUTPUT | jq -r '.data.txhash'`
-            if [ -z "$TXHASH" ] && [ "$TXHASH" = null ]
-            then
+            TXHASH=$(echo $CMD_OUTPUT | jq -r '.data.txhash')
+            if [ -z "$TXHASH" ] && [ "$TXHASH" = null ]; then
                 echo "ERROR no tx found!" >&2
                 echo $CMD_OUTPUT >&2
                 return 1
             fi
 
             # query tx for making sure it succeeds!
-            QUERY_OUTPUT=`query_tx --chain $CHAIN --tx $TXHASH --max-call-limit $MAX_CALL_LIMIT`
+            QUERY_OUTPUT=$(query_tx --chain $CHAIN --tx $TXHASH --max-call-limit $MAX_CALL_LIMIT)
             EXIT_CODE=$?
-            if [ $EXIT_CODE != 0 ]
-            then
+            if [ $EXIT_CODE != 0 ]; then
                 echo "$QUERY_OUTPUT" >&2
                 return $EXIT_CODE >&2
             fi
-            INITIAL_CMD=`echo $CMD_OUTPUT | jq -r '.cmd' | sed 's/"/\\\\"/g'` # escape double quotes
-            RESULT=`echo $QUERY_OUTPUT | jq "{ cmd: \"$INITIAL_CMD\", data: .data, id: \"$COLLECTION\"}"`
+            INITIAL_CMD=$(echo $CMD_OUTPUT | jq -r '.cmd' | sed 's/"/\\\\"/g') # escape double quotes
+            RESULT=$(echo $QUERY_OUTPUT | jq "{ cmd: \"$INITIAL_CMD\", data: .data, id: \"$COLLECTION\"}")
             echo $RESULT | jq
             return 0
         else
-            if [ -z "$SYMBOL" ]
-            then
+            if [ -z "$SYMBOL" ]; then
                 echo "--symbol is required" >&2
                 return 1
             fi
-            if [ -z "$NAME" ]
-            then
+            if [ -z "$NAME" ]; then
                 echo "--name is required" >&2
                 return 1
             fi
@@ -218,36 +233,33 @@ function create_collection() {
 --from "$FROM" \
 --fees "$CLI_MAX_FEES" \
 -b "$CLI_BROADCAST_MODE" --yes" \
-"$( [ ! -z $URI ] && echo "--uri $URI" || echo "")" \
-"$( [ ! -z "$DATA" ] && echo "--data '$DATA'" || echo "")" \
-"$( [ ! -z "$DESCRIPTION" ] && echo "--description=\"$DESCRIPTION\"" || echo "")" # --description not documented in CLI...
+                "$([ ! -z $URI ] && echo "--uri $URI" || echo "")" \
+                "$([ ! -z "$DATA" ] && echo "--data '$DATA'" || echo "")" \
+                "$([ ! -z "$DESCRIPTION" ] && echo "--description=\"$DESCRIPTION\"" || echo "")" # --description not documented in CLI...
 
-            CMD_OUTPUT=`execute_cli "$CMD"`
+            CMD_OUTPUT=$(execute_cli "$CMD")
             EXIT_CODE=$?
-            if [ $EXIT_CODE != 0 ]
-            then
+            if [ $EXIT_CODE != 0 ]; then
                 echo "$QUERY_OUTPUT" >&2
                 return "$EXIT_CODE" >&2
             fi
-            TXHASH=`echo $CMD_OUTPUT | jq -r '.data.txhash'`
-            if [ -z "$TXHASH" ] && [ "$TXHASH" = null ]
-            then
+            TXHASH=$(echo $CMD_OUTPUT | jq -r '.data.txhash')
+            if [ -z "$TXHASH" ] && [ "$TXHASH" = null ]; then
                 echo "ERROR no tx found!" >&2
                 echo "$CMD_OUTPUT" >&2
                 return 1
             fi
 
             # query tx for making sure it succeeds!
-            QUERY_OUTPUT=`query_tx --chain $CHAIN --tx $TXHASH --max-call-limit $MAX_CALL_LIMIT`
+            QUERY_OUTPUT=$(query_tx --chain $CHAIN --tx $TXHASH --max-call-limit $MAX_CALL_LIMIT)
             EXIT_CODE=$?
-            if [ $EXIT_CODE != 0 ]
-            then
+            if [ $EXIT_CODE != 0 ]; then
                 echo "$QUERY_OUTPUT" >&2
                 return "$EXIT_CODE"
             fi
-            COLLECTION=`jq --argjson j "$QUERY_OUTPUT" -r -n '$j.data.tx.body.messages[0].id'`
-            INITIAL_CMD=`echo $CMD_OUTPUT | jq -r '.cmd' | sed 's/"/\\\\"/g'` # escape double quotes
-            RESULT=`echo $QUERY_OUTPUT | jq "{ cmd: \"$INITIAL_CMD\", data: .data, id: \"$COLLECTION\"}"`
+            COLLECTION=$(jq --argjson j "$QUERY_OUTPUT" -r -n '$j.data.tx.body.messages[0].id')
+            INITIAL_CMD=$(echo $CMD_OUTPUT | jq -r '.cmd' | sed 's/"/\\\\"/g') # escape double quotes
+            RESULT=$(echo $QUERY_OUTPUT | jq "{ cmd: \"$INITIAL_CMD\", data: .data, id: \"$COLLECTION\"}")
             echo $RESULT | jq
             return 0
         fi
